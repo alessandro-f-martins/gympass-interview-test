a9117a941264f83d7377a35e43a80ae298e9dc68
Alessandro Martins <alessandro.f.martins@gmail.com>
2019-07-17 19:40:05 -0300

    Primeiro commit

---
+++ corrida_gympass.py
@@ -0,0 +1,225 @@
+# coding: utf-8
+""" Módulo corrida_gympass:
+
+Este módulo implementa um avaliador simples de resultados para corridas de
+Kart, de acordo com o solicitado para o teste prático do processo seletivo
+para a posição de Tech Lead Engineer na Gympass.
+
+"""
+
+import re
+import pandas as pd
+
+
+class AvaliaCorrida:
+    """Avalia o resultado de provas de Kart.
+
+    AvaliaCorrida(nome_arq)
+
+    Args:
+        nome_arq (str): inicia o avaliador de corridas com os dados do
+        arquivo `nome_arq`. Este arquivo deve ser um TSV (tab-separated values)
+        no seguinte formato:
+
+        Linhas de cabeçalho:
+            - Hora: momento da tomada de volta, no formato HH:MM:SS.mmmm
+            - Piloto: referência ao piloto, no formato
+                `<cod_piloto> – <nome_piloto>`
+                NOTA: o caracter `–` é um EN-dash; não confundir com um hífen
+                comum (`-`)
+            - Nº Volta: número da volta
+            - Tempo Volta: tomada de tempo da volta, no formato MM:SS.mmmm
+            - Velocidade média da volta: no formato dd.dddd ou dd,dddd
+
+        O avaliador aceita arquivos com separadores de campos alternando entre
+        tabs e espaços (para as linhas de dados) e tabs _ou_ um ou mais espaços
+        (para o cabeçalho).
+
+        Exemplo:
+
+        Hora   Piloto  Nº Volta  Tempo Volta  Velocidade média da volta
+        23:49:08.277 038 – F.MASSA 1  1:02.852  44,275
+
+    """
+
+    def __init__(self, nome_arq):
+        """Vide descrição da classe."""
+        self.df_corrida = self.carrega_dados(nome_arq)
+
+    def _tempo_em_milis(self, tempo):
+        """Método privado. Converte tempo no formato MM:SS:mmm para
+            milissegundos."""
+        tempo_lista = re.split(':|\.', tempo)
+        return int(tempo_lista[0])*60000 + int(tempo_lista[1])*1000 + \
+            int(tempo_lista[2])
+
+    def _milis_em_tempo_str(self, tempo_milis):
+        """Método privado. Converte tempo em milissegundos em uma string com
+           o formato 'MMminSSsmmm'."""
+        return str(tempo_milis // 60000) + 'min' + \
+            str((tempo_milis % 60000) // 1000) + 's' + \
+            str(tempo_milis % 1000)
+
+    def _calcula_colocacao(self):
+        """Método privado. Calcula a ordem de chegada e o total de tempo de
+           prova em milissegundos."""
+        return self.df_corrida.groupby('Codigo Piloto').sum().\
+            sort_values('Tempo Volta em milis', ascending=True).\
+            filter(items=['Codigo Piloto', 'Tempo Volta em milis'])
+
+    def carrega_dados(self, nome_arq):
+        """Carrega o avaliador com um novo arquivo de dados.
+
+        Args:
+            nome_arq (str): Nome do arquivo de dados
+
+        """
+
+        primeira_linha = None
+        dados_lista = []
+        # O arquivo dado como exemplo possui muitas irregularidades na
+        # separação dos campos. Para respeitar a proposta do teste, assume-se
+        # que o programa deva ser capaz de tratar tais pontos, e toda limpeza
+        # de dados é realizada pelo código, sem preparação ou preparação
+        # externa.
+        with open(nome_arq, 'r') as arq:
+            for linha in arq:
+                if not primeira_linha:
+                    # Continuando com a premissa de não-interferência externa
+                    # no arquivo de dados fornecido, percebeu-se também
+                    # inconsistências nos nomes dos pilotos (Ex.: "F.MASSA" e
+                    # "F.MASS"). Desta forma, optou-se por fazer a referência
+                    # aos pilotos por seu código. Para isso, o parsing do
+                    # arquivo separa o código do nome e o coloca em um campo
+                    # interno em separado.
+                    primeira_linha = re.split('  +|\t+', linha.rstrip())
+                    primeira_linha.insert(1, 'Codigo Piloto')
+                    # Inserção de um novo campo com o tempo de volta em
+                    # milissegundos para cálculos.
+                    primeira_linha.append('Tempo Volta em milis')
+                else:
+                    nova_linha = re.split('  +| *\t+ *| – ', linha.rstrip())
+                    nova_linha[-1] = float(nova_linha[-1].replace(',', '.'))
+                    # Inserção de um novo campo com o tempo de volta em
+                    # milissegundos para cálculos.
+                    nova_linha.append(self._tempo_em_milis(
+                        nova_linha[primeira_linha.index('Tempo Volta')]))
+                    dados_lista.append(nova_linha)
+
+        return pd.DataFrame(columns=primeira_linha, data=dados_lista)
+
+    def resultado_corrida(self):
+        """Retorna o resultado da corrida (prova) como um DataFrame pandas.
+
+        Returns:
+            Resultado da prova como um DataFrame pandas, com os seguintes
+            dados:
+                - Posição Chegada
+                - Codigo Piloto
+                - Nome Piloto
+                - Tempo Total de Prova
+                - Qtde Voltas Completadas
+
+        """
+
+        # Obtendo do DataFrame com os tempos de prova totais em milissegundos
+        # na ordem de término de prova (vide método _calcula_colocacao()).
+        colocacao = self._calcula_colocacao()
+        # Formatando o tempo de prova para exibição e renomeando o nome da
+        # coluna de acordo com os requerimentos
+        colocacao['Tempo Total de Prova'] = colocacao['Tempo Volta em milis'].\
+            apply(self._milis_em_tempo_str)
+        # Retirando a coluna 'Tempo Total de Prova em milis' para retorno e
+        # exibição.
+        colocacao.drop(columns=['Tempo Volta em milis'], inplace=True)
+        # Obtendo do DataFrame com a última volta completa de cada piloto
+        volta_completa = self.df_corrida.groupby('Codigo Piloto').max().\
+            sort_values('Nº Volta').filter(items=['Codigo Piloto', 'Nº Volta'])
+        # Renomeando o nome da coluna de acordo com os requerimentos
+        volta_completa.rename(
+            columns={'Nº Volta': 'Qtde Voltas Completadas'}, inplace=True)
+        # Realizando o join dos resultados
+        res_corrida = colocacao.join(volta_completa,
+                                     on='Codigo Piloto').reset_index()
+        # Renumerando o índice para refletir a posição de chegada e nomeando-o
+        # de acordo com os requerimentos
+        res_corrida.index += 1
+        res_corrida.index.name = 'Posição Chegada'
+        return res_corrida
+
+    def melhor_volta(self, no_piloto=None):
+        """Retorna a volta mais rápida da prova ou a melhor volta de um piloto.
+
+        Args:
+            no_piloto: código do piloto, ou None para a melhor volta da prova
+            (default: None).
+
+        Returns:
+            Tupla com os seguintes dados:
+            (<cod_piloto>, <nome_piloto>, <numero_volta>, <tempo_volta>)
+
+        """
+
+        if no_piloto:
+            # Subconjunto do DataFrame relativo ao piloto.
+            temp_df = self.df_corrida[self.df_corrida['Codigo Piloto']
+                                      == no_piloto]
+        else:
+            # Fazendo uma cópia de trabalho do DataFrame original, para não
+            # alterá-lo.
+            temp_df = self.df_corrida.copy(deep=True)
+
+        # Encontrando o registro com o menor valor para o tempo de volta
+        _melhor_volta = temp_df[temp_df['Nº Volta'] == temp_df.loc[temp_df[
+            'Tempo Volta em milis'].idxmin()]['Nº Volta']].reset_index()
+
+        return (_melhor_volta.loc[0, 'Codigo Piloto'],
+                _melhor_volta.loc[0, 'Piloto'],
+                _melhor_volta.loc[0, 'Nº Volta'],
+                _melhor_volta.loc[0, 'Tempo Volta'])
+
+    def velocidade_media(self):
+        """Retorna a velocidade média de cada piloto.
+
+        Returns:
+            Dicionário com os seguintes dados:
+            {<cod_piloto_1>: <velocidade_piloto_1>,...
+             <cod_piloto_n>: <velocidade_piloto_n>}
+
+        """
+
+        # Agrupando os registros por piloto, calculando a média das
+        # velocidades, ordenando por esta média, transformando em array Numpy
+        # e retornando por código do piloto como dicionário
+        medias_vel = self.df_corrida.groupby('Codigo Piloto').mean().\
+            sort_values('Velocidade média da volta', ascending=False).\
+            filter(items=['Codigo Piloto', 'Velocidade média da volta']).\
+            reset_index().values
+        return {a[0]: a[1] for a in medias_vel}
+
+    def diferencas_pilotos(self):
+        """Retorna as diferenças de tempo de chegada entre o vencedor e os
+        demais pilotos. Os tempos são dados como strings no formato
+        "MMminSSsddd".
+
+        Obs.: o vencedor é retornado na primeira posição da lista com valor 0
+        (`0min0s0`).
+
+        Returns:
+            Dicionário com os seguintes dados:
+            {<cod_piloto_vencedor>: <tempo_piloto_vencedor>,...
+             <cod_piloto_2>: <velocidade_piloto_2>}
+             <cod_piloto_n>: <velocidade_piloto_n>}
+
+        """
+
+        # Obtendo a colocação dos pilotos em ordem com seus tempos totais de
+        # prova em milissegundos e transformando em array Numpy (vide método
+        # _calcula_colocacao()
+        resultado_corrida_voltas = self._calcula_colocacao().\
+            reset_index().values
+        # Extraindo o tempo do vencedor
+        tempo_ganhador = resultado_corrida_voltas[0][1]
+        # Retornando as diferenças.
+        return {a[0]: self._milis_em_tempo_str(a[1] - tempo_ganhador)
+                for a in resultado_corrida_voltas}
---
+++ testa_corrida_gympass.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+from corrida_gympass import AvaliaCorrida
+from pprint import pprint
+
+pilotos_para_teste = {
+    '002': 'Kimi Raikkonen',
+    '011': 'Sebastian Vettel',
+    '015': 'Fernando Alonso',
+    '023': 'Mark Webber',
+    '033': 'Rubens Barrichello',
+    '038': 'Felipe Massa'
+}
+
+if __name__ == "__main__":
+
+    avalia_corrida = AvaliaCorrida('./corredores.data')
+
+    print('\nTestando o módulo de avaliação de corridas para a Gympass: \n')
+    print('Serão usados os seguintes códigos para os pilotos:\n')
+    for piloto in pilotos_para_teste:
+        print('%s: %s' % (pilotos_para_teste[piloto], piloto))
+
+    resultado_prova = avalia_corrida.resultado_corrida()
+    resultado_prova.insert(1, 'Nome Piloto', resultado_prova['Codigo Piloto'].
+                           apply(lambda x: pilotos_para_teste[x]))
+
+    melhor_volta_pilotos = {pilotos_para_teste[no_piloto]:
+                            avalia_corrida.melhor_volta(no_piloto)
+                            for no_piloto in pilotos_para_teste}
+
+    melhor_volta_corrida = avalia_corrida.melhor_volta()
+    vel_medias = avalia_corrida.velocidade_media()
+    dif_pilotos = avalia_corrida.diferencas_pilotos()
+
+    print("\nResultado da prova:\n")
+    pprint(resultado_prova)
+    print('\n ==========================\n')
+
+    print("Melhor volta de cada piloto:\n")
+    for piloto in melhor_volta_pilotos:
+        print("%s: %sª volta (Tempo: %s)" % (piloto,
+                                             melhor_volta_pilotos[piloto][2],
+                                             melhor_volta_pilotos[piloto][3]))
+    print('\n ==========================\n')
+
+    print("Melhor volta da prova:\n")
+    print("%s: %sª volta (Tempo: %s)" %
+          (pilotos_para_teste[melhor_volta_corrida[0]],
+           melhor_volta_corrida[2], melhor_volta_corrida[3]))
+    print('\n ==========================\n')
+
+    print("Velocidades médias:\n")
+    for piloto in vel_medias:
+        print("%s: %.2f km/h" % (pilotos_para_teste[piloto],
+                                 vel_medias[piloto]))
+    print('\n ==========================\n')
+
+    vencedor = next(iter(dif_pilotos))
+    dif_pilotos.pop(vencedor)
+    print("Diferenças de tempo para o 1º colocado (%s):\n" %
+          pilotos_para_teste[vencedor])
+    for piloto in dif_pilotos:
+        print("%s: %s" % (pilotos_para_teste[piloto], dif_pilotos[piloto]))
+    print('\n ==========================\n')
